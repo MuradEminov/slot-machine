73bf2fe83d5f47bcb73ffd85069585d79c5285d4
MuradEminov <murademinov88@gmail.com>
2021-06-13 18:01:33 +0400

    Negative balance disable spin feature is done

--- handleBalance.js
+++ handleBalance.js
@@ -1,13 +1,14 @@
-function handleBalance(balance) {
+function handleChange(balance) {
   var spinBtn = document.getElementById('gogogo2');
   var infoText = document.getElementById('spin-info-text');
 
-  if (balance <= 0) {
-    spinBtn.disabled = true;
-    infoText.innerHTML = 'Please, insert money to start.';
-  }
   if (balance > 0) {
     spinBtn.disabled = false;
     infoText.innerHTML = 'GO SPIN!';
-  
+  }
+  if (balance > 5000) {
+    infoText.innerHTML =
+      'NO MORE THAN 5000$ IS ALLOWED TO ENTER' + '\n' + '(ENTER LESS MONEY)';
+    spinBtn.disabled = true;
+  }
 }
