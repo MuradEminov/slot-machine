cb7ca97d68f47f7881e01762229df4b5827fca19
MuradEminov <murademinov88@gmail.com>
2021-06-13 18:31:07 +0400

    Js files import/export problem

--- handleBalance.js
+++ handleBalance.js
@@ -1,14 +1,17 @@
 function handleChange(balance) {
+  var gameBalance = balance; // Current balance
   var spinBtn = document.getElementById('gogogo2');
   var infoText = document.getElementById('spin-info-text');
 
-  if (balance > 0) {
+  if (gameBalance > 0) {
     spinBtn.disabled = false;
     infoText.innerHTML = 'GO SPIN!';
   }
-  if (balance > 5000) {
+  if (gameBalance > 5000) {
     infoText.innerHTML =
-      'NO MORE THAN 5000$ IS ALLOWED TO ENTER' + '\n' + '(ENTER LESS MONEY)';
+      'NO MORE THAN 5000$ IS ALLOWED TO ENTER (PLEASE TRY AGAIN)';
     spinBtn.disabled = true;
   }
 }
+
+export { handleChange };
